61e9740b72a996251b3a822192b277401e5d1558
amit1870 <amitxvf@gmail.com>
2021-09-27 14:33:06 +0530

    Adding helper file

--- utils/helper.py
+++ utils/helper.py
@@ -5,9 +5,10 @@ This module will contain helping functions.
 import contextlib
 import sys
 
-
 class DummyFile(object):
     def write(self, x): pass
+
+    def flush(self, x): pass
 
 @contextlib.contextmanager
 def silent_std_out():
