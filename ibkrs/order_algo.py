8f8c46cdc4d771535550ad5e8f59ea936c299123
amit1870 <amitxvf@gmail.com>
2021-10-15 15:51:04 +0530

    Adding figure to BB

--- ibkrs/order_algo.py
+++ ibkrs/order_algo.py
@@ -7,7 +7,29 @@ import numpy as np
 
 pd.options.mode.chained_assignment = None  # default='warn'
 
-def bolliner_bands(data_list, period, lower_factor=0.5, upper_factor=0.5):
+FILE_PATH = "/home/ec2-user/virenv"
+
+def get_signal(data):
+    """ Function to get Sell or Buy signal."""
+    buy_signal = [] # buy list
+    sell_signal = [] # sell list
+
+    for i in range(len(data['Close'])):
+        # Sell
+        if data['Close'][i] > data['Upper'][i]:
+            buy_signal.append(np.nan)
+            sell_signal.append(data['Close'][i])
+        # Buy
+        elif data['Close'][i] < data['Lower'][i]:
+            sell_signal.append(np.nan)
+            buy_signal.append(data['Close'][i])
+        else:
+            buy_signal.append(np.nan)
+            sell_signal.append(np.nan)
+
+    return buy_signal, sell_signal
+
+def bolliner_bands(data_list, period, lower_factor=0.5, upper_factor=0.5, plot=True):
     ''' Bollinger Bands.'''
     df = pd.DataFrame(data_list)
 
@@ -25,5 +47,72 @@ def bolliner_bands(data_list, period, lower_factor=0.5, upper_factor=0.5):
 
     # create a new data frame
     new_df = df[period-1:]
+
+    if plot:
+        column_list = ['Close', 'SMA', 'Upper', 'Lower']
+
+        df[column_list].plot(figsize=(12.2,6.4))
+
+        plt.title('Bollinger Bands')
+
+        plt.ylabel('USD Price ($)')
+
+        figure = "{}/fig1.jpg".format(FILE_PATH)
+        plt.savefig("{}".format(figure))
+
+        # plot and shade the area between the two Bollinger bands
+        fig = plt.figure(figsize=(12.2,6.4)) # width = 12.2" and height = 6.4"
+
+        # Add the subplot
+        ax = fig.add_subplot(1,1,1) # number of rows, cols and index
+
+        # Get the index values of the DataFrame
+        x_axis = df.index
+
+        # plot and shade the area between the upper band and the lower band Grey
+        ax.fill_between(x_axis, df['Upper'], df['Lower'], color='grey')
+
+        # plot the Closing Price and Moving Average
+        ax.plot(x_axis, df['Close'], color='gold', lw=3, label = 'Close Price') #lw = line width
+
+        ax.plot(x_axis, df['SMA'], color='blue', lw=3, label = 'Simple Moving Average')
+
+        # Set the Title & Show the Image
+        ax.set_title('Bollinger Bands')
+        ax.set_xlabel('Date')
+        ax.set_ylabel('USD Price ($)')
+        plt.xticks(rotation = 45)
+        ax.legend()
+
+        figure = "{}/fig2.jpg".format(FILE_PATH)
+        plt.savefig("{}".format(figure))
+
+        # create new columns for the buy and sell signals
+        buy_signal, sell_signal = get_signal(new_df)
+        new_df['Buy'] = buy_signal
+        new_df['Sell'] = sell_signal
+
+        fig = plt.figure(figsize=(12.2,6.4))
+        ax = fig.add_subplot(1,1,1)
+        x_axis = new_df.index
+
+        # plot and shade the area between the upper band and the lower band Grey
+        ax.fill_between(x_axis, new_df['Upper'], new_df['Lower'], color='grey')
+
+        # plot the Closing Price and Moving Average
+        ax.plot(x_axis, new_df['Close'], color='gold', lw=3, label = 'Close Price',alpha = 0.5)
+        ax.plot(x_axis, new_df['SMA'], color='blue', lw=3, label = 'Moving Average',alpha = 0.5)
+        ax.scatter(x_axis, new_df['Buy'] , color='green', lw=3, label = 'Buy',marker = '^', alpha = 1)
+        ax.scatter(x_axis, new_df['Sell'] , color='red', lw=3, label = 'Sell',marker = 'v', alpha = 1)
+
+        # set the Title and Show the Image
+        ax.set_title('Bollinger Bands')
+        ax.set_xlabel('Date')
+        ax.set_ylabel('USD Price ($)')
+        plt.xticks(rotation = 45)
+        ax.legend()
+
+        figure = "{}/fig3.jpg".format(FILE_PATH)
+        plt.savefig("{}".format(figure))
 
     return new_df
